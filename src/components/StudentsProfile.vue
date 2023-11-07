2053d606d3f0fffda5ca3e2b71dd6f9592df6fe5
angelicaaa <128461122+eynjeeeeel@users.noreply.github.com>
2023-11-07 06:27:49 -0800

    individual

--- src/components/StudentsProfile.vue
+++ src/components/StudentsProfile.vue
@@ -13,125 +13,149 @@
   
   <script>
   const columns = [
-    {
-      name: 'name',
-      required: true,
-      label: 'Name of Students',
-      align: 'left',
-      field: row => row.name,
-      format: val => `${val}`,
-      sortable: true
-    },
-    { name: 'calories', align: 'center', label: 'Calories', field: 'calories', sortable: true },
-    { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
-    { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
-    { name: 'protein', label: 'Protein (g)', field: 'protein' },
-    { name: 'sodium', label: 'Sodium (mg)', field: 'sodium' },
-    { name: 'calcium', label: 'Calcium (%)', field: 'calcium', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
-    { name: 'iron', label: 'Iron (%)', field: 'iron', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) }
-  ]
+  {
+    name: 'name',
+    required: true,
+    label: 'Name of Students',
+    align: 'left',
+    field: row => row.name,
+    format: val => `${val}`,
+    sortable: true
+  },
+  { 
+    name: 'Age', 
+    align: 'center', 
+    label: 'Age', 
+    field: 'Age',
+    sortable: true 
+  },
+  { 
+    name: 'Address', 
+    label: 'Address', 
+    field: 'Address',
+    sortable: true
+  },
+  {
+    name: 'Birthday', 
+    label: 'Birthday',
+    field: 'Birthday',
+    
+  },
+  { 
+    name: 'Gender', 
+    label: 'Gender', 
+    field: 'Gender',
+    sortable: true
+  },
+  {
+    name: 'Parent/Guardian',
+    label: 'Parent/Guardian',
+    field: 'Parent', 
+    
+  },
+  {
+    name: 'Relationship',
+    label: 'Relationship',
+    field: 'Relationship',
+  },
+  {
+    name: 'Contact No.',
+    label: 'Contact No.', 
+    field: 'Contact',
+    
+  },
+  {
+    name: 'Coach',
+    label: 'Coach',
+    field: 'Coach',
+    sortable: true,
+    sort: (a, b) => parseInt(a, 10) - parseInt(b, 10)
+  },
+  {
+    name: 'Section',
+    label: 'Section',
+    field: 'Section',
+    sortable: true,
+    sort: (a, b) => parseInt(a, 10) - parseInt(b, 10)
+  },
+  {
+    name: 'ID',
+    label: 'ID',
+    field: 'ID',
+  },
+  {
+    name: 'Sponsor',
+    label: 'Sponsor',
+    field: 'Sponsor',
+  },
+  {
+    name: 'Staff',
+    label: 'Staff',
+    field: 'Staff',
+  }
+]
   
   const rows = [
-    {
-      name: 'Frozen Yogurt',
-      calories: 159,
-      fat: 6.0,
-      carbs: 24,
-      protein: 4.0,
-      sodium: 87,
-      calcium: '14%',
-      iron: '1%'
+  {
+      name: 'Leynard Chequito',
+      Age: 21,
+      Address: 'Puerto Galera',
+      Birthday: 'December 31, 2002',
+      Gender: 'Male',
+      Parent: 'Mrs. Chequito',
+      Relationship: 'Mother',
+      Contact: '09123456789',
+      Coach: 'Dyze ',
+      Section: '1',
+      ID:'1245',
+      Sponsor:' Eya',
+      Staff:'Tere',
     },
     {
-      name: 'Ice cream sandwich',
-      calories: 237,
-      fat: 9.0,
-      carbs: 37,
-      protein: 4.3,
-      sodium: 129,
-      calcium: '8%',
-      iron: '1%'
+      name: 'Angelica Perez',
+      Age: 19,
+      Address: 'Pinahan, Naujan',
+      Birthday: 'July 17, 2003',
+      Gender: 'Female',
+      Parent: 'Venus Perez',
+      Relationship: 'Mother',
+      Contact: '09124638382',
+      Coach: 'Kas',
+      Section: '2',
+      ID:'9872',
+      Sponsor:'Dyze',
+      Staff:'Cheqs',
     },
     {
-      name: 'Eclair',
-      calories: 262,
-      fat: 16.0,
-      carbs: 23,
-      protein: 6.0,
-      sodium: 337,
-      calcium: '6%',
-      iron: '7%'
+      name: 'Kasandra Jurquia',
+      Age: 20,
+      Address: 'Calapan City',
+      Birthday: 'August 20, 2003',
+      Gender: 'Female',
+      Parent: 'Mrs. Jurqia',
+      Relationship: 'Mother',
+      Contact: '09152837453',
+      Coach: 'Leynard',
+      Section: '3',
+      ID:'8254',
+      Sponsor:'Dyze',
+      Staff:'May',
     },
     {
-      name: 'Cupcake',
-      calories: 305,
-      fat: 3.7,
-      carbs: 67,
-      protein: 4.3,
-      sodium: 413,
-      calcium: '3%',
-      iron: '8%'
+      name: 'Dyze Malaluan',
+      Age: 23,
+      Address: 'Victoria',
+      Birthday: 'December 01, 2000',
+      Gender: 'Male',
+      Parent: 'Mr. Malaluan',
+      Relationship: 'Father',
+      Contact: '09167893548',
+      Coach: 'Eya',
+      Section: '4',
+      ID:'9321',
+      Sponsor:'Kas',
+      Staff:'Leynard',
     },
-    {
-      name: 'Gingerbread',
-      calories: 356,
-      fat: 16.0,
-      carbs: 49,
-      protein: 3.9,
-      sodium: 327,
-      calcium: '7%',
-      iron: '16%'
-    },
-    {
-      name: 'Jelly bean',
-      calories: 375,
-      fat: 0.0,
-      carbs: 94,
-      protein: 0.0,
-      sodium: 50,
-      calcium: '0%',
-      iron: '0%'
-    },
-    {
-      name: 'Lollipop',
-      calories: 392,
-      fat: 0.2,
-      carbs: 98,
-      protein: 0,
-      sodium: 38,
-      calcium: '0%',
-      iron: '2%'
-    },
-    {
-      name: 'Honeycomb',
-      calories: 408,
-      fat: 3.2,
-      carbs: 87,
-      protein: 6.5,
-      sodium: 562,
-      calcium: '0%',
-      iron: '45%'
-    },
-    {
-      name: 'Donut',
-      calories: 452,
-      fat: 25.0,
-      carbs: 51,
-      protein: 4.9,
-      sodium: 326,
-      calcium: '2%',
-      iron: '22%'
-    },
-    {
-      name: 'KitKat',
-      calories: 518,
-      fat: 26.0,
-      carbs: 65,
-      protein: 7,
-      sodium: 54,
-      calcium: '12%',
-      iron: '6%'
-    }
   ]
   
   export default {
