263818f2982bdbc372ae43be731d5593f0a428fe
kasJurquia <144781016+kasJurquia@users.noreply.github.com>
2023-11-07 14:26:22 +0800

    editt

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -82,6 +82,15 @@
                         Sponsors
                     </q-item-section>
                 </q-item>
+                <q-item clickable v-ripple>
+                    <q-item-section avatar>
+                        <q-icon name="class" />
+                    </q-item-section>
+
+                    <q-item-section>
+                        Section
+                    </q-item-section>
+                </q-item>
             </q-list>
         </q-scroll-area>
     </q-drawer>
