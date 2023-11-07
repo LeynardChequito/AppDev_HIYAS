f31cef53c60ed9cce91a71ac230807b5647c2dd8
LeynardChequito <144776361+LeynardChequito@users.noreply.github.com>
2023-11-08 00:36:24 +0800

    UpcomingEvents Update

--- src/views/EventsView.vue
+++ src/views/EventsView.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
     <Birthdays />
+    <UpcomingEvents />
 </div>
 </template>
 <script>
 // @ is an alias to /src
 import Birthdays from '@/components/Birthdays.vue'
+import UpcomingEvents from '@/components/UpcomingEvents.vue'
 export default {
     name: 'events',
     components: {
         Birthdays,
+        UpcomingEvents
     }
 }
 </script>
