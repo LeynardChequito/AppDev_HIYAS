e73a88eb4d739a7fcef70234094883069d8efdb9
LeynardChequito <144776361+LeynardChequito@users.noreply.github.com>
2023-11-14 00:36:54 +0800

    Adjustment

--- src/components/Announcements.vue
+++ src/components/Announcements.vue
@@ -1,31 +1,112 @@
 <template>
-  <div class="q-pa-md row items-start q-gutter-md">
-    <q-card class="my-card">
-      <q-card-section class="bg-grey-8 text-white">
-        <div class="text-h6 text-center">Our Changing Planet</div>
-        <div class="text-subtitle2 text-center">by John Doe</div>
-      </q-card-section>
+  <div class="centered-card"><q-card class="my-card" style="width: 800px;">
+  
+  <q-card-section>
+    <div class="text-h3 text-center">Announcements</div>
+  </q-card-section>
 
-      <q-card-actions vertical align="center">
-        <div class="text-h3 text-center">Our Changing Planet</div>
-        <div class="text-h3 text-center">by John Doe</div>
-      </q-card-actions>
-    </q-card>
-  </div>
+  <q-separator />
+
+  <q-card-section class="q-pt-none">
+  
+    <q-scroll-area style="height: 400px;">
+      <q-timeline>
+        <q-timeline-entry
+          v-for="announcement in announcements" 
+          :key="announcement.id"
+          :title="announcement.title"
+          :subtitle="announcement.date"
+        >
+          {{ announcement.description }}
+        </q-timeline-entry>
+      </q-timeline>
+    </q-scroll-area>
+    
+  </q-card-section>
+
+</q-card> 
+</div>
+   
 </template>
 
 <script>
 export default {
-  setup () {
+  data() {
     return {
-      lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      announcements: [
+        {
+          id: 1,  
+          title: 'Site Maintenance',
+          date: 'January 1, 2020',
+          description: 'The site will be undergoing maintenance between 3-5pm PST.'
+        },
+        {
+          id: 2,
+          title: 'New Feature Release', 
+          date: 'January 15, 2020',
+          description: 'We have released a new feature for analytics reporting. See the documentation for more details.'
+        },
+        {
+          id: 3,
+          title: 'Service Disruption',
+          date: 'January 28, 2020',
+          description: 'There will be intermittent service disruptions between 2-4am PST due to network upgrades.'
+        },
+        {
+          id: 4,
+          title: 'App Update',
+          date: 'February 5, 2020',
+          description: 'A new version of the app has been released. Please update for the latest features and bug fixes.'
+        },
+        {  
+          id: 5,
+          title: 'Billing Reminder',
+          date: 'February 15, 2020', 
+          description: 'This is a reminder that yearly billing will be charged on March 1st. Please update your payment information.'
+        },
+        {
+          id: 6,
+          title: 'Server Migration',
+          date: 'March 10, 2020',
+          description: 'Our servers will be undergoing migration March 15-17. Minimal downtime is expected.'
+        },
+        {
+          id: 7,
+          title: 'Holiday Hours',
+          date: 'April 1, 2020',
+          description: 'Support will be limited December 23-25 and December 31 - January 2 for the holidays.'
+        },
+        {
+          id: 8, 
+          title: 'Password Reset',
+          date: 'May 5, 2020',
+          description: 'If you have forgotten your password, you can reset it here. A reset link will be emailed.'
+        },
+        {
+          id: 9,
+          title: '2FA Changes',
+          date: 'June 20, 2020',
+          description: 'Two-factor authentication (2FA) methods have been updated. Review the security page for more information.'
+        }
+      ]
     }
-  }
+  } 
 }
 </script>
+<style>
+.centered-card {
+  display: flex;
+  justify-content: center;
+  align-items: center; 
+}
 
-<style lang="sass" scoped>
-.my-card
-  width: 100%
-  max-width: 250px
+.my-card {
+  max-width: 800px;
+  margin: 0 auto; 
+  
+}
+
+.q-card {
+  background: linear-gradient(to right, #273430, #185a9d); 
+}
 </style>
