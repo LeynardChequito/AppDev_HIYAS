515c83852037a8f8e7297eb90bcf05c91fdfda59
kasJurquia <144781016+kasJurquia@users.noreply.github.com>
2023-11-16 11:54:14 +0800

    eventssss

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -23,22 +23,25 @@
             transition-next="jump-up"
           >
             <q-tab-panel name="2019/02/01">
-              <div class="text-h4 q-mb-md">2019/02/01</div>
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+              <div class="text-h4 q-mb-md">Cleanup Party</div>
+              <p>Get ready for a day of teamwork, fun, and environmental responsibility! We're excited to invite students, parents, teachers, and staff to our School Cleanup Bash. </p>
+              <p>Let's join forces to give our campus a fresh makeover and instill a sense of pride in our shared learning environment.</p>
             </q-tab-panel>
   
             <q-tab-panel name="2019/02/05">
-              <div class="text-h4 q-mb-md">2019/02/05</div>
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+              <div class="text-h4 q-mb-md">Tailgate Party</div>
+              <p>Get ready for the ultimate school spirit experience! Join us for a lively Tailgate Party before the big game. This is a fantastic opportunity for students, parents, teachers, 
+                and staff to come together, enjoy delicious food, play games, and build excitement for the upcoming sports event. Let\'s create lasting memories, showcase our school pride, and cheer on our teams to victory!
+                 Don\'t forget to wear your school colors and bring your energy to make this Tailgate Party a memorable celebration</p>
+              
             </q-tab-panel>
   
             <q-tab-panel name="2019/02/06">
-              <div class="text-h4 q-mb-md">2019/02/06</div>
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+              <div class="text-h4 q-mb-md">Relay Day</div>
+              <p>Get ready for an exhilarating day of camaraderie and competition at our annual Relay Day! This event brings together students, teachers, and staff for a day of fun, teamwork, and physical activities. 
+                Participants will engage in a variety of relay races, challenges, and team-building exercises. It's a fantastic opportunity to foster school spirit, promote a healthy lifestyle, and create lasting memories. 
+                Whether you're cheering from the sidelines or actively participating, Relay Day is all about bringing our school community together for a day of joy and celebration. Let's make it a day to remember</p>
+              
             </q-tab-panel>
           </q-tab-panels>
         </template>
