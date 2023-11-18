2253d28e7a1349cb876b2f7fe155e8267c1adf8b
LeynardChequito <144776361+LeynardChequito@users.noreply.github.com>
2023-11-19 00:52:31 +0800

    Manage Student Info V.2

--- src/components/Student/MSTable.vue
+++ src/components/Student/MSTable.vue
@@ -1,86 +1,99 @@
 <template>
-    <div class="q-pa-md">
-      <q-btn color="primary" label="Add New" @click="addNew"></q-btn>
+  <div>
+    <q-btn color="primary" @click="addRow" class="q-mt-md">
+      Add New
+    </q-btn>
+
+    <q-table
+      class="my-sticky-header-last-column-table"
+      flat
+      bordered
+      :rows="info"
+      :columns="columns"
+      row-key="id"
+    >
+          <template v-slot:body="props">
+            <q-tr :props="props">
+              <q-td v-for="col in columns" :key="col.name" :props="props">
+                {{ props.row[col.field] }}
+              </q-td>
+              <q-td>
+                <q-btn
+                  icon="visibility"
+                  round
+                  flat
+                  @click="handleRowClick(props.row)"
+                ></q-btn>
+                <q-btn
+                  icon="edit"
+                  round
+                  flat
+                  @click="editRow(props.row)"
+                ></q-btn>
+                <q-btn
+                  icon="delete"
+                  round
+                  flat
+                  @click="deleteRow(props.row)"
+                ></q-btn>
+              </q-td>
+            </q-tr>
+          </template>
+        </q-table>
+      </div>
   
-      <q-table class="my-sticky-header-last-column-table" flat bordered :rows="info" :columns="columns" row-key="id">
-        <template v-slot:body="props">
-          <q-tr :props="props">
-            <q-td v-for="col in columns" :key="col.name" :props="props">
-              {{ props.row[col.name] }}
-            </q-td>
-            <q-td>
-              <q-btn icon="visibility" round flat @click="handleRowClick(props.row)"></q-btn>
-              <q-btn icon="edit" round flat @click="editRow(props.row)"></q-btn>
-              <q-btn icon="delete" round flat @click="deleteRow(props.row)"></q-btn>
-            </q-td>
-          </q-tr>
-        </template>
-      </q-table>
-  
-      <q-dialog v-model="editModal" persistent>
-        <q-card>
-          <q-card-section>
-            <q-form @submit="saveChanges" class="q-gutter-md">
-              <q-input v-model="editedData.firstname" label="First Name"></q-input>
-              <q-input v-model="editedData.lastname" label="Last Name"></q-input>
-              <q-input v-model="editedData.birthday" label="Birthday"></q-input>
-              <q-input v-model="editedData.age" label="Age"></q-input>
-              <q-input v-model="editedData.gender" label="Gender"></q-input>
-              <q-input v-model="editedData.address" label="Address"></q-input>
-              <q-input v-model="editedData.contact_info" label="Contact Info"></q-input>
-              <q-input v-model="editedData.guardian_parents" label="Parent/Guardian"></q-input>
-              <q-input v-model="editedData.school" label="School"></q-input>
-              <q-input v-model="editedData.grade_level" label="Grade Level"></q-input>
-              <q-input v-model="editedData.coach" label="Coach"></q-input>
-              <q-input v-model="editedData.section" label="Section"></q-input>
-              <q-input v-model="editedData.sponsor" label="Sponsor"></q-input>
-              <q-input v-model="editedData.staff" label="Staff"></q-input>
-              <q-input v-model="editedData.fam_number" label="Number in Family" align="center"></q-input>
-  
-              <div class="q-mt-md">
-                <q-btn type="submit" color="primary" label="Save Changes"></q-btn>
-                <q-btn color="red" label="Cancel" @click="cancelEdit"></q-btn>
-              </div>
-            </q-form>
-          </q-card-section>
-        </q-card>
-      </q-dialog>
-  
-      <q-dialog v-model="addModal" persistent>
-        <q-card>
-          <q-card-section>
-            <q-form @submit="addNewData" class="q-gutter-md">
-              <q-input v-model="newData.firstname" label="First Name"></q-input>
-              <q-input v-model="newData.lastname" label="Last Name"></q-input>
-              <q-input v-model="newData.birthday" label="Birthday"></q-input>
-              <q-input v-model="newData.age" label="Age"></q-input>
-              <q-input v-model="newData.gender" label="Gender"></q-input>
-              <q-input v-model="newData.address" label="Address"></q-input>
-              <q-input v-model="newData.contact_info" label="Contact Info"></q-input>
-              <q-input v-model="newData.guardian_parents" label="Parent/Guardian"></q-input>
-              <q-input v-model="newData.school" label="School"></q-input>
-              <q-input v-model="newData.grade_level" label="Grade Level"></q-input>
-              <q-input v-model="newData.coach" label="Coach"></q-input>
-              <q-input v-model="newData.section" label="Section"></q-input>
-              <q-input v-model="newData.sponsor" label="Sponsor"></q-input>
-              <q-input v-model="newData.staff" label="Staff"></q-input>
-              <q-input v-model="newData.fam_number" label="Number in Family" align="center"></q-input>
-  
-              <div class="q-mt-md">
-                <q-btn type="submit" color="primary" label="Add Data"></q-btn>
-                <q-btn color="red" label="Cancel" @click="cancelAdd"></q-btn>
-              </div>
-            </q-form>
-          </q-card-section>
-        </q-card>
-      </q-dialog>
+      <div class="edit-form" v-if="editMode">
+       
+    <div class="form-container q-gutter-md">
+        <h4 style="margin-top: 1px; text-align: center;">Edit Student Information</h4> 
+      <div class="row">
+        <div v-for="col in columns" :key="col.name" class="col-3 form-group">
+          <label v-if="col.label !== 'Action'" :for="'edit-' + col.field">
+            {{ col.label }}:
+          </label>
+          <input
+            v-if="col.label !== 'Action'"
+            :type="col.field === 'birthday' ? 'date' : 'text'"
+            v-model="editForm[col.field]"
+            :id="'edit-' + col.field"
+          />
+        </div>
+      </div>
+      <div class="form-actions">
+        <button @click="saveEdit">Save Changes</button>
+        <button @click="cancelEdit">Cancel</button>
+      </div>
     </div>
-  </template>
+  </div>
+
+  <!-- Add Form -->
+  <div class="add-form" v-if="addMode">
+    <div class="form-container q-gutter-md">
+        <h4 style="margin-top: 1px; text-align: center;">Add Another Student Information</h4> 
+      <div class="row">
+        <div v-for="col in columns" :key="col.name" class="col-3 form-group">
+          <label v-if="col.label !== 'Action'" :for="'add-' + col.field">
+            {{ col.label }}:
+          </label>
+          <input
+            v-if="col.label !== 'Action'"
+            :type="col.field === 'birthday' ? 'date' : 'text'"
+            v-model="addForm[col.field]"
+            :id="'add-' + col.field"
+          />
+        </div>
+      </div>
+      <div class="form-actions">
+        <button @click="saveAdd">Add Data</button>
+        <button @click="cancelAdd">Cancel</button>
+      </div>
+    </div>
+  </div>
+</template>
   
   <script>
   import axios from 'axios';
   
-  // Define the columns outside the data method
   const columns = [
     { name: 'id_number', label: 'Id Number', align: 'left', field: 'id_number', sortable: true },
     { name: 'lastname', label: 'Lastname', align: 'left', field: 'lastname', sortable: true },
@@ -90,7 +103,7 @@
     { name: 'gender', label: 'Gender', align: 'left', field: 'gender', sortable: true },
     { name: 'address', label: 'Address', align: 'left', field: 'address', sortable: true },
     { name: 'contact_info', label: 'Contact Info', align: 'left', field: 'contact_info', sortable: true },
-    { name: 'guardian_parents', label: 'Parent/Guardian', align: 'left', field: 'guardian_parents', sortable: true },
+    { name: 'guardianParents', label: 'Parent/Guardian', align: 'left', field: 'guardianParents', sortable: true },
     { name: 'school', label: 'School', align: 'left', field: 'school', sortable: true },
     { name: 'grade_level', label: 'Grade Level', align: 'left', field: 'grade_level', sortable: true },
     { name: 'coach', label: 'Coach', align: 'left', field: 'coach', sortable: true },
@@ -105,15 +118,17 @@
     data() {
       return {
         info: [],
-        editModal: false,
-        addModal: false,
-        editedData: {},
-        newData: {},
+        editMode: false,
+        editForm: {},
+        addMode: false,
+        addForm: {},
       };
     },
+  
     created() {
       this.getInfo();
     },
+  
     methods: {
       async getInfo() {
         try {
@@ -123,70 +138,82 @@
           console.error('Error fetching data:', error);
         }
       },
+  
       handleRowClick(row) {
-        // Assuming the id property is present in the row object
         const id = row.id;
-        // Navigate to the new route
         this.$router.push(`/reports/students/${id}`);
       },
+  
+      navigateToManage() {
+        this.$router.push('/manage/students');
+      },
+  
       editRow(row) {
-        // Set the data to be edited
-        this.editedData = { ...row };
-        // Show the edit modal
-        this.editModal = true;
+        this.editMode = true;
+        this.editForm = { ...row };
       },
-      saveChanges() {
-        // Assuming you have an endpoint for updating data
-        axios
-          .put(`updatestudentdata/${this.editedData.id}`, this.editedData)
-          .then(response => {
-            // Update the local data with the changes
-            const index = this.info.findIndex(item => item.id === this.editedData.id);
-            if (index !== -1) {
-              this.$set(this.info, index, this.editedData);
-            }
   
-            // Close the edit modal
-            this.editModal = false;
-          })
-          .catch(error => {
-            console.error('Error updating data:', error);
-          });
-      },
-      cancelEdit() {
-        // Reset the edited data and close the edit modal
-        this.editedData = {};
-        this.editModal = false;
-      },
-      deleteRow(row) {
-        // Implement your delete logic here
-        // You may want to ask for confirmation or perform additional checks before deletion
-      },
-      addNew() {
-        this.newData = {}; // Clear previous data
-        this.addModal = true;
-      },
-      addNewData() {
-        // Assuming you have an endpoint for adding new data
-        axios
-          .post('addstudentdata', this.newData)
-          .then(response => {
-            // Update the local data with the new data
-            this.info.push(response.data);
-  
-            // Close the add modal
-            this.addModal = false;
-          })
-          .catch(error => {
-            console.error('Error adding data:', error);
-          });
-      },
-      cancelAdd() {
-        // Reset the new data and close the add modal
-        this.newData = {};
-        this.addModal = false;
-      },
+      saveEdit() {
+      // Implement save logic, update the row in the info array
+      // Call an API, update in the database, etc.
+      // Assuming you have a unique identifier like 'id' for each row
+      const editedRowIndex = this.info.findIndex(
+        (row) => row.id === this.editForm.id
+      );
+      if (editedRowIndex !== -1) {
+        // Update the existing row in the info array
+        this.info.splice(editedRowIndex, 1, { ...this.editForm });
+      }
+
+      // Reset editMode after saving
+      this.editMode = false;
     },
+  
+      cancelEdit() {
+        this.editMode = false;
+      },
+  
+      addRow() {
+        this.addMode = true;
+        // Initialize addForm fields if needed
+      },
+  
+      saveAdd() {
+      // Implement save logic, add a new row to the info array
+      // Call an API, insert in the database, etc.
+      // Assuming you have a unique identifier like 'id' for each row
+      const newId = this.info.length + 1; // Generate a new unique id
+      const newRow = { id: newId, ...this.addForm };
+
+      // Add the new row to the info array
+      this.info.push(newRow);
+
+      // Reset addMode after saving
+      this.addMode = false;
+        },
+      cancelAdd() {
+        this.addMode = false;
+      },
+  
+      deleteRow(row) {
+      // Add a confirmation dialog before deletion
+      const isConfirmed = window.confirm('Are you sure you want to delete this data?');
+      if (!isConfirmed) {
+        return; // Cancel deletion if not confirmed
+      }
+
+      // Implement delete logic, remove the row from the info array
+      // Call an API, delete from the database, etc.
+      // Assuming you have a unique identifier like 'id' for each row
+      const deletedRowIndex = this.info.findIndex(
+        (item) => item.id === row.id
+      );
+      if (deletedRowIndex !== -1) {
+        // Remove the row from the info array
+        this.info.splice(deletedRowIndex, 1);
+      }
+    },
+},
     computed: {
       columns() {
         return columns;
@@ -194,65 +221,96 @@
     },
   };
   </script>
+  
   <style lang="scss" scoped>
-  .my-sticky-header-last-column-table {
-    /* height or max-height is important */
-    /* specifying max-width so the example can highlight the sticky column on any browser window */
+    td:last-child {
+      background-color: #EEEE;
+    }
+  
+    tr th {
+      position: sticky;
+      z-index: 9;
+      background: #EEEEEE;
+    }
+  
+    thead tr:last-child th {
+      top: 48px;
+      z-index: 9;
+    }
+  
+    thead tr:first-child th {
+      top: 0;
+      z-index: 1;
+    }
+  
+    tr:last-child th:last-child {
+      z-index: 3;
+    }
+  
+    td:last-child {
+      z-index: 1;
+    }
+  
+    td:last-child, th:last-child {
+      position: sticky;
+      right: 0;
+    }
+  
+    tbody {
+      scroll-margin-top: 48px;
+    }
+  
+    .q-form {
+      width: 500px;
+      height: 500px;
+      margin: 0 auto;
+    }
+  
+    .q-mt-md {
+      margin-top: 20px;
+      margin-left: 20px;
+      margin-bottom: 5px;
+    }
+  
+    .form-container {
+    background-color: #fff;
+    padding: 30px; /* Adjust the padding as needed */
+    border-radius: 8px;
+    box-shadow: 0 2px 4px rgba(52, 52, 52, 0.1);
+    margin: 1px 0;
   }
 
-  td:last-child {
-    /* bg color is important for td; just specify one */
-    background-color: #EEEE;
+  label {
+    display: block;
+    margin-bottom: 5px;
+    font-weight: bold;
   }
 
-  tr th {
-    position: sticky;
-    /* higher than z-index for td below */
-    z-index: 2;
-    /* bg color is important; just specify one */
-    background: #EEEEEE;
+  input {
+    width: 75%;
+    padding: 6px; /* Adjust the padding for smaller input forms */
+    font-size: 12px; /* Adjust the font size as needed */
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
   }
 
-  /* this will be the loading indicator */
-  thead tr:last-child th {
-    /* height of all previous header rows */
-    top: 48px;
-    /* highest z-index */
-    z-index: 3;
+  .form-actions {
+    margin-top: 10px;
   }
 
-  thead tr:first-child th {
-    top: 0;
-    z-index: 1;
+  button {
+    margin-right: 10px; /* Adjust the margin as needed */
+    padding: 9px; /* Adjust the padding as needed */
+    font-size: 12px; /* Adjust the font size as needed */
+    border: none;
+    border-radius: 12px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
   }
 
-  tr:last-child th:last-child {
-    /* highest z-index */
-    z-index: 3;
-  }
-
-  td:last-child {
-    z-index: 1;
-  }
-
-  td:last-child, th:last-child {
-    position: sticky;
-    right: 0;
-  }
-
-  /* prevent scrolling behind sticky top row on focus */
-  tbody {
-    /* height of all previous header rows */
-    scroll-margin-top: 48px;
-  }
-
-  .q-form {
-    width: 800px; /* Adjust the form width as needed */
-    height: auto;
-    margin: 0 auto;
-  }
-
-  .q-mt-md {
-    margin-top: 20px; /* Adjust the margin as needed */
+  button:hover {
+    background-color:rgb(102, 58, 233);
+    color: #ffffff;
   }
 </style>
