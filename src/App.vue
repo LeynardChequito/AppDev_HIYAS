cce11a6db84ee87ee276506ca6aec4a135c82fb4
Dyzeeeeee <128461122+eynjeeeeel@users.noreply.github.com>
2023-12-10 23:44:43 +0800

    Commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <q-layout view="lHr LpR fFr" class="bg-grey-10">
 
-    <Navbar />
+    <!-- Only show the Navbar component if the user role is 'Staff' -->
+    <!-- <Navbar/> -->
+    <Navbar  />
+
     <Drawer />
     <q-resize-observer @resize="onResize" />
     <q-page-container>
@@ -12,16 +15,15 @@
 </template>
 
 <script setup>
-import Navbar from '@/components/Admin/Navbar.vue'
-import Drawer from '@/components/Admin/Drawer.vue'
-
+import Navbar from '@/components/Admin/Navbar.vue';
+import Drawer from '@/components/Admin/Drawer.vue';
 </script>
 
 <style scoped>
-.sticky{
+.sticky {
   position: sticky;
   top: 0;
-  background-color: white; /* Add a background color for visibility */
-  width: 100%; 
+  background-color: white;
+  width: 100%;
 }
 </style>
